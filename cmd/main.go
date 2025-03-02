afe906a70c8305e10f4c7b5a1b4f58e6db1e891a
Vishal Anarase <iamvishalanarase@gmail.com>
2025-03-02 12:36:39 +0530

    User mux router and refactor to func

--- cmd/main.go
+++ cmd/main.go
@@ -7,13 +7,13 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 	"github.com/vishalanarase/pod-webhook-mutator/pkg/webhook"
 )
 
-func main() {
-	fmt.Println("Hello, World!")
-
+// GetTLSConfig returns a TLS configuration for the server.
+func GetTLSConfig() *tls.Config {
 	// Load the Server certificate and key
 	crt, err := os.ReadFile("certs/server.crt")
 	if err != nil {
@@ -43,18 +43,30 @@ func main() {
 		},
 	}
 
-	// Add the handlers for the healthz and readyz endpoints
-	http.HandleFunc("/healthz", webhook.Healthz)
-	http.HandleFunc("/readyz", webhook.Readyz)
+	return tlsConfig
+}
+
+// GetRouter returns a new router with the handlers for the healthz and readyz endpoints.
+func GetRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/healthz", webhook.Healthz)
+	r.HandleFunc("/readyz", webhook.Readyz)
+
+	return r
+}
+
+func main() {
+	fmt.Println("Hello, World!")
 
 	// Server configuration
 	server := &http.Server{
 		Addr:      ":443",
-		TLSConfig: tlsConfig,
+		TLSConfig: GetTLSConfig(),
+		Handler:   GetRouter(),
 	}
 
 	fmt.Println("Starting server on :443")
-	err = server.ListenAndServeTLS("certs/server.crt", "certs/server.key")
+	err := server.ListenAndServeTLS("certs/server.crt", "certs/server.key")
 	if err != nil {
 		logrus.Fatal(err)
 	}
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/vishalanarase/pod-webhook-mutator
 
 go 1.23.5
 
-require github.com/sirupsen/logrus v1.9.3
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/sirupsen/logrus v1.9.3
+)
 
 require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
